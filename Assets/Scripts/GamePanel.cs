e3cb23d1f3f5e6b60576ec430e81734ae669710c
KaslGame <kaslgame@mail.ru>
2024-07-21 23:16:36 +0300

    Init

--- Assets/Scripts/GamePanel.cs
+++ Assets/Scripts/GamePanel.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class GamePanel : MonoBehaviour
 {
@@ -9,31 +10,90 @@ public class GamePanel : MonoBehaviour
 
     [SerializeField] private AudioMixerGroup _mixer;
 
+    [SerializeField] private AudioSource _blizzardSource;
+    [SerializeField] private AudioSource _bubbleSource;
+    [SerializeField] private AudioSource _burnSource;
+
+    [SerializeField] private Toggle _volumeToggle;
+
+    [SerializeField] private Button _blizzardButton;
+    [SerializeField] private Button _bubbleButton;
+    [SerializeField] private Button _burnButton;
+
+    [SerializeField] private Slider _musicSlider;
+    [SerializeField] private Slider _effectsSlider;
+    [SerializeField] private Slider _masterSlider;
+
     private int _multipler = 20;
 
-    public void ToggleMasterVolume(bool enabled)
+    private float _lastVolume;
+    private bool _isMute = false;
+
+    private void OnEnable()
+    {
+        _volumeToggle.onValueChanged.AddListener(ToggleMasterVolume);
+        _blizzardButton.onClick.AddListener(_blizzardSource.Play);
+        _bubbleButton.onClick.AddListener(_bubbleSource.Play);
+        _burnButton.onClick.AddListener(_burnSource.Play);
+        _musicSlider.onValueChanged.AddListener(ChangeMusicVolume);
+        _effectsSlider.onValueChanged.AddListener(ChangeEffectsVolume);
+        _masterSlider.onValueChanged.AddListener(ChangeMasterVolume);
+    }
+
+    private void OnDisable()
+    {
+        _volumeToggle.onValueChanged.RemoveListener(ToggleMasterVolume);
+        _blizzardButton.onClick.RemoveListener(_blizzardSource.Play);
+        _bubbleButton.onClick.RemoveListener(_bubbleSource.Play);
+        _burnButton.onClick.RemoveListener(_burnSource.Play);
+        _musicSlider.onValueChanged.RemoveListener(ChangeMusicVolume);
+        _effectsSlider.onValueChanged.RemoveListener(ChangeEffectsVolume);
+        _masterSlider.onValueChanged.RemoveListener(ChangeMasterVolume);
+    }
+
+    public void ToggleMasterVolume(bool active)
     {
         int minValue = -80;
-        int maxValue = 0;
 
-        if (enabled)
-            _mixer.audioMixer.SetFloat(MasterVolume, maxValue);
+        if (active)
+        {
+            _mixer.audioMixer.SetFloat(MasterVolume, _lastVolume);
+            _isMute = true;
+        }
         else
+        {
             _mixer.audioMixer.SetFloat(MasterVolume, minValue);
+            _isMute = false;
+        }
+
+        ChangeInteractableSliders(_isMute);
     }
 
-    public void ChangeMasterVolume(float value)
+    private void ChangeInteractableSliders(bool state)
     {
-        _mixer.audioMixer.SetFloat(MasterVolume, Mathf.Log10(value) * _multipler);
+        _masterSlider.interactable = state;
+        _musicSlider.interactable = state;
+        _effectsSlider.interactable = state;
     }
 
-    public void ChangeMusicVolume(float value)
+    private void ChangeVolume(string name, float value)
     {
-        _mixer.audioMixer.SetFloat(MusicVolume, Mathf.Log10(value) * _multipler);
+        _mixer.audioMixer.SetFloat(name, Mathf.Log10(value) * _multipler);
+        _lastVolume = Mathf.Log10(value) * _multipler;
     }
 
-    public void ChangeEffectsVolume(float value)
+    private void ChangeMasterVolume(float value)
     {
-        _mixer.audioMixer.SetFloat(EffectsVolume, Mathf.Log10(value) * _multipler);
+        ChangeVolume(MasterVolume, value);
+    }
+
+    private void ChangeMusicVolume(float value)
+    {
+        ChangeVolume(MusicVolume, value);
+    }
+
+    private void ChangeEffectsVolume(float value)
+    {
+        ChangeVolume(EffectsVolume, value);
     }
 }
